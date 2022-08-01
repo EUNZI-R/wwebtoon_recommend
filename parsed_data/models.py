1045ea9c210f47901a782e174eeff69c88f3a21c
simsosimso <simsamsom33@gnu.ac.kr>
2022-08-01 16:52:24 +0900

    first commit

--- parsed_data/models.py
+++ parsed_data/models.py
@@ -8,3 +8,17 @@ class BlogData(models.Model):
 
     def __str__(self):
         return self.title
+    
+class KakaopageData(models.Model):
+    title = models.CharField(max_length=200)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+class KakaowebtoonData(models.Model):
+    title = models.CharField(max_length=200)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
