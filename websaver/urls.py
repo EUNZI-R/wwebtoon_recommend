2e79e869555fe3f9b76e23113502b35290dc5339
simsosimso <simsamsom33@gnu.ac.kr>
2022-08-05 22:51:48 +0900

    save webtoon information as CSV file

--- websaver/urls.py
+++ websaver/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from parsed_data import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gcsv/naver',views.psg),
+    path('gcsv/kakaoweb',views.psg),
+    path('gcsv/kakaopage',views.psg),
 ]
